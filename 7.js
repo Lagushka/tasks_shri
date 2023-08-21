18fe9d5ae1732b283934d4ca56085ca85da64552
Borislav <borislav.bondarev@gmail.com>
2023-08-21 21:44:42 +0300

    erathosphen

---
+++ 7.js
@@ -0,0 +1,32 @@
+function erathosfen(n) {
+	const arr = [];
+	for (let i = 2; i <= n; i++) {
+		arr[i] = true;
+	}
+
+	let step = 2;
+
+	while (step <= n) {
+		for (let i = step * 2; i <= n; i += step) {
+			arr[i] = false;
+		}
+
+		let i = step + 1;
+		while (i <= n && !arr[i]) {
+			i++;
+		}
+		step = i;
+	}
+
+	const result = [];
+
+	for (let i = 2; i < arr.length; i++) {
+		if (arr[i]) {
+			result.push(i);
+		}
+	}
+
+	return result;
+}
+
+// console.log(erathosfen(400));
